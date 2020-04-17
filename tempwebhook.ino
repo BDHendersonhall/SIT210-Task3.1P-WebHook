21a04d106543817f871da65c08cfc9842110ccc3
BDHendersonhall <BDHendersonhall@users.noreply.github.com>
2020-04-17 23:29:42 +0800

    Add files via upload

--- tempwebhook.ino
+++ tempwebhook.ino
@@ -3,40 +3,30 @@
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
-unsigned long lastPublish = 0; // A time variable to know when we published to the cloud last
-double temp = 0; // Current temperature
-double humidity = 0; // Current humidity
-char tempC[15]; // empty var to store temp as a char so we can publish to .publish(), since it requires a char and not an int
-char humidityC[15]; // empty var to store humidity as a char so we can publish to .publish(), since it requires a char and not an int
+double temperature = 0; // temperature measurement
+double humidity = 0; // humidity measurement
+char temperatureCh[15]; // .publish() requires a char and not an int we make a char variable to put the measurement in
+char humidityCh[15]; // .publish() requires a char and not an int we make a char variable to put the measurement in
         
-int publishInterval = 30; // Temperature and humidity publish frequency, seconds
-
 DHT dht(DHTPIN, DHTTYPE);
 
-bool shouldPublish() {
-        return Time.now() > lastPublish + publishInterval;
-}
-
-void publishAction() {
-        Particle.publish("Temperature", tempC, PRIVATE); // Publish the temperature as an event
-        Particle.publish("Humidity", humidityC, PRIVATE); // Publish the humidity as an event
-        lastPublish = Time.now();
-}
-
 void setup() {
-    Particle.variable("Temperature", tempC);    // Enables console to pull current Temperature value
-    Particle.variable("Humidity", humidityC);   // Enables console to pull current Humidity value
+    Particle.variable("Temperature", temperatureCh);    // Enables console to pull current Temperature value
+    Particle.variable("Humidity", humidityCh);   // Enables console to pull current Humidity value
 }
 
 void loop() {
-    if(shouldPublish()) {
-        publishAction();
-    }
+
+    temperature = dht.getTempCelcius();
     humidity = dht.getHumidity();
-    temp = dht.getTempCelcius();
-    if(temp!=0) {
-        sprintf(tempC, "%.1f", temp); // assign temp as a char to tempC
-        sprintf(humidityC, "%.1f", humidity); // assign humidity as a char to humidityC
+
+    if(temperature!=0) {
+        sprintf(temperatureCh, "%.1f", temperature); // assign temperature as a char to temperatureCh
+        sprintf(humidityCh, "%.1f", humidity); // assign humidity as a char to humidityCh
+        
+        Particle.publish("Temperature", temperatureCh, PRIVATE); // Publish the temperature as an event
+        Particle.publish("Humidity", humidityCh, PRIVATE); // Publish the humidity as an event  
     }
-    delay(100);
+  
+    delay(30000); //wait for 30 seconds
 }
